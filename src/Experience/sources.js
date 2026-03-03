98930c089898a95a4f9c24c901a1a452cddff84c
jeslif <jeslif2020@gmail.com>
2026-03-03 15:30:14 +0530

    clean up!!!!! and link

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -13,16 +13,6 @@ export default [
         ]
     },
     {
-        name: 'grassColorTexture',
-        type: 'texture',
-        path: 'textures/dirt/color.jpg'
-    },
-    {
-        name: 'grassNormalTexture',
-        type: 'texture',
-        path: 'textures/dirt/normal.jpg'
-    },
-    {
         name: 'foxModel',
         type: 'gltfModel',
         path: 'models/Fox/glTF/Fox.gltf'
